b12579df15444d6bcc2bc5ec4580967aa5c93c57
shawnxuchina <50402888+shawnxuchina@users.noreply.github.com>
2019-12-22 16:41:31 +0800

    Update newmath.hpp

--- xxx/newmath/newmath.hpp
+++ xxx/newmath/newmath.hpp
@@ -2,6 +2,6 @@
 
 #include <cmath>
 
-const double pi = 3.1415926;
-const double _e = 2.7182818;
+const long double pi = 3.1415926;
+const long double _e = 2.7182818;
 const long long _c = 299792458;
